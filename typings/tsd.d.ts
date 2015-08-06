c1c1700848feab4971d52f5ff22498f9c256a45c
Rene Eichhorn <rene.eichhorn@mni.thm.de>
2015-08-06 14:29:56 +0200

    typings fix

---
+++ typings/tsd.d.ts
@@ -0,0 +1,14 @@
+
+/// <reference path="hapi/hapi.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="sequelize/sequelize.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="twig/twig.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request/request.d.ts" />
+/// <reference path="object-hash/object-hash.d.ts" />
